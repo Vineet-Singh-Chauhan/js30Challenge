5a5feb9393962b7e74a2d15ee0bf1c3ee095049f
Vineet-Singh-Chauhan <chauhansvineet@gmail.com>
2023-09-18 22:30:22 +0530

    day 3 css variables

---
+++ 03_cssVariables/assets/scripts/main.js
@@ -0,0 +1,15 @@
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdate() {
+  console.log(this.dataset);
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+inputs.forEach((e) => {
+  e.addEventListener("change", handleUpdate);
+  e.addEventListener("mousemove", handleUpdate);
+});
