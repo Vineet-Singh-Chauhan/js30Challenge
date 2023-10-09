4899bbcc11d7bc983ec5f3591e7cd7df902b9eb8
Vineet-Singh-Chauhan <chauhansvineet@gmail.com>
2023-10-09 19:34:08 +0530

    upto day 21 completed

---
+++ 21_geolocation/assets/scripts/main.js
@@ -0,0 +1,12 @@
+const arrow = document.querySelector(".arrow");
+const speed = document.querySelector(".speed-value");
+
+navigator.geolocation.watchPosition(
+  (data) => {
+    speed.textContent = data.coords.speed || 0;
+    arrow.computedStyleMap.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => {
+    console.error(err);
+  }
+);
